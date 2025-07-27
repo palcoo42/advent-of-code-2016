20bc9fd46e6ffc773c121db8368e42ba3d0e00b2
palcoo42 <pavol.ondrusek@gmail.com>
2025-07-27 19:59:42 +0200

    Day 05: Increase performance by using buffer array instead of Vectors and Strings

--- day_05_how_about_a_nice_game_of_chess/src/puzzle/hacker.rs
+++ day_05_how_about_a_nice_game_of_chess/src/puzzle/hacker.rs
@@ -1,4 +1,8 @@
 const PASSWORD_LENGTH: usize = 8;
+const HEX_DIGITS: &[char] = &[
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
+];
+const ASCII_DIGIT_0: u8 = 48;
 
 pub struct Hacker {
     door_id: String,
@@ -11,33 +15,65 @@ impl Hacker {
         }
     }
 
+    #[inline]
+    fn number_to_u8(mut number: usize, array: &mut [u8; 8]) -> usize {
+        let mut index = 0;
+
+        loop {
+            if number == 0 {
+                break;
+            }
+
+            let code = (number % 10) as u8;
+            number /= 10;
+
+            array[index] = code + ASCII_DIGIT_0;
+            index += 1;
+        }
+
+        array[..index].reverse();
+        index
+    }
+
     pub fn crack_password(&self) -> String {
         let mut pwd = String::with_capacity(PASSWORD_LENGTH);
-        let mut buffer: Vec<u8> = Vec::with_capacity(self.door_id.len() + 12);
+
+        // Buffer for whole data to hash
+        let mut buffer = [0_u8; 32];
+        let mut buffer_len;
+
+        // Buffer for number only
         let mut number: usize = 1;
+        let mut buffer_number = [0_u8; 8];
+        let mut buffer_number_len;
+
+        // Insert door ir, it is same for all iterations
+        buffer[..self.door_id.len()].copy_from_slice(self.door_id.as_bytes());
 
         while pwd.len() != PASSWORD_LENGTH {
-            // Prepare buffer
-            buffer.clear();
-            buffer.extend_from_slice(self.door_id.as_bytes());
-            buffer.extend_from_slice(number.to_string().as_bytes());
+            // Prepare buffer, door id is already filled
+            // Convert manually number to chars
+            buffer_number_len = Self::number_to_u8(number, &mut buffer_number);
+            buffer_len = self.door_id.len() + buffer_number_len;
+
+            buffer[self.door_id.len()..buffer_len]
+                .copy_from_slice(&buffer_number[..buffer_number_len]);
 
             // Calculate hash
-            let digest = md5::compute(&buffer);
+            let digest = md5::compute(&buffer[..buffer_len]);
 
             // Find five zeros
             // Digest is [u8; 16]
             // byte 0 -> two zeros
             // byte 1 -> two zeros
             // byte 2 -> one zero (higher byte)
-            let five_zeros =
-                digest.0[0] == 0 && digest.0[1] == 0 && ((digest.0[2] & 0xF0) >> 4) == 0;
+            let five_zeros = digest.0[0] == 0 && digest.0[1] == 0 && (digest.0[2] & 0xF0) == 0;
 
             if five_zeros {
                 // 6th number is lower part of 3rd byte
-                let code = digest.0[2] & 0x0F;
-                let code = format!("{code:x}");
-                pwd.push_str(&code);
+                let code = (digest.0[2] & 0x0F) as usize;
+                let code = HEX_DIGITS[code];
+                pwd.push(code);
             }
 
             number += 1;
@@ -50,17 +86,30 @@ impl Hacker {
         // _ means no pwd yet in the position
         const EMPTY: char = '_';
         let mut pwd = [EMPTY; PASSWORD_LENGTH];
-        let mut buffer: Vec<u8> = Vec::with_capacity(self.door_id.len() + 12);
+
+        // Buffer for whole data to hash
+        let mut buffer = [0_u8; 32];
+        let mut buffer_len;
+
+        // Buffer for number only
         let mut number: usize = 1;
+        let mut buffer_number = [0_u8; 8];
+        let mut buffer_number_len;
+
+        // Insert door ir, it is same for all iterations
+        buffer[..self.door_id.len()].copy_from_slice(self.door_id.as_bytes());
 
         while pwd.iter().any(|c| c == &EMPTY) {
-            // Prepare buffer
-            buffer.clear();
-            buffer.extend_from_slice(self.door_id.as_bytes());
-            buffer.extend_from_slice(number.to_string().as_bytes());
+            // Prepare buffer, door id is already filled
+            // Convert manually number to chars
+            buffer_number_len = Self::number_to_u8(number, &mut buffer_number);
+            buffer_len = self.door_id.len() + buffer_number_len;
+
+            buffer[self.door_id.len()..buffer_len]
+                .copy_from_slice(&buffer_number[..buffer_number_len]);
 
             // Calculate hash
-            let digest = md5::compute(&buffer);
+            let digest = md5::compute(&buffer[..buffer_len]);
 
             // Find five zeros
             // Digest is [u8; 16]
@@ -74,16 +123,13 @@ impl Hacker {
                 // 6th number is lower part of 3rd byte
                 let position = (digest.0[2] & 0x0F) as usize;
 
-                // 7th number is higher part of 4th byte
-                let code = (digest.0[3] & 0xF0) >> 4;
-                let code = format!("{code:x}");
-
                 // Write only on first occurence + skip invalid position
                 if position < PASSWORD_LENGTH && pwd[position] == EMPTY {
-                    pwd[position] = code
-                        .chars()
-                        .next()
-                        .expect("Code shall contain single hex character");
+                    // 7th number is higher part of 4th byte
+                    let code = ((digest.0[3] & 0xF0) >> 4) as usize;
+                    let code = HEX_DIGITS[code];
+
+                    pwd[position] = code;
                 }
             }
 
@@ -106,5 +152,25 @@ mod tests {
     #[test]
     fn test_crack_password_advanced() {
         assert_eq!(&Hacker::new("abc").crack_password_advanced(), "05ace8e3");
+    }
+
+    #[test]
+    fn test_number_to_u8() {
+        let mut array = [0_u8; 8];
+
+        Hacker::number_to_u8(123, &mut array);
+        assert_eq!(
+            array,
+            [
+                1 + ASCII_DIGIT_0,
+                2 + ASCII_DIGIT_0,
+                3 + ASCII_DIGIT_0,
+                0,
+                0,
+                0,
+                0,
+                0
+            ]
+        );
     }
 }
